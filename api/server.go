f3baef21900b1656feec4fc1ee3a1e7a285787b4
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-02-09 16:34:17 +0100

    adds support for all events

--- api/server.go
+++ api/server.go
@@ -10,13 +10,14 @@ import (
 // NewRouter creates a new HTTP Server
 func NewRouter() *chi.Mux {
 	router := chi.NewRouter()
-	router.Get("/status", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("OK"))
+	router.Get("/status", func(w http.ResponseWriter, _ *http.Request) {
+		w.Write([]byte("OK")) // nolint: errcheck, gosec
 	})
 
 	return router
 }
 
+// NewHTTPServer creates a new HTTP server
 func NewHTTPServer(port int, handler http.Handler) *http.Server {
 	return &http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
--- events/event.go
+++ events/event.go
@@ -13,21 +13,76 @@ type Type string
 
 // defines various Event Types
 const (
-	MessageCreateEventType Type = "message_create"
-	MessageUpdateEventType Type = "message_update"
-	MessageDeleteEventType Type = "message_delete"
+	ChannelCreateType            Type = "channel_create"
+	ChannelDeleteType            Type = "channel_delete"
+	ChannelPinsUpdateType        Type = "channel_pins_update"
+	ChannelUpdateType            Type = "channel_update"
+	GuildBanAddType              Type = "guild_ban_add"
+	GuildBanRemoveType           Type = "guild_ban_remove"
+	GuildCreateType              Type = "guild_create"
+	GuildDeleteType              Type = "guild_delete"
+	GuildEmojisUpdateType        Type = "guild_emojis_update"
+	GuildMemberAddType           Type = "guild_member_add"
+	GuildMemberRemoveType        Type = "guild_member_remove"
+	GuildMemberUpdateType        Type = "guild_member_update"
+	GuildMembersChunkType        Type = "guild_members_chunk"
+	GuildRoleCreateType          Type = "guild_role_create"
+	GuildRoleDeleteType          Type = "guild_role_delete"
+	GuildRoleUpdateType          Type = "guild_role_update"
+	GuildUpdateType              Type = "guild_update"
+	GuildIntegrationsUpdateType  Type = "guild_integrations_update"
+	MessageCreateType            Type = "message_create"
+	MessageDeleteType            Type = "message_delete"
+	MessageDeleteBulkType        Type = "message_delete_bulk"
+	MessageReactionAddType       Type = "message_reaction_add"
+	MessageReactionRemoveType    Type = "message_reaction_remove"
+	MessageReactionRemoveAllType Type = "message_reaction_remove_all"
+	MessageUpdateType            Type = "message_update"
+	PresenceUpdateType           Type = "presence_update"
+	UserUpdateType               Type = "user_update"
+	VoiceStateUpdateType         Type = "voice_state_update"
+	VoiceServerUpdateType        Type = "voice_server_update"
+	WebhooksUpdateType           Type = "webhooks_update"
 )
 
 // Event represents an Event
+// TODO: event ID or Key
 type Event struct {
 	Type       Type
 	ReceivedAt time.Time
 	BotUserID  string
 
 	// discordgo event data
-	MessageCreate *discordgo.MessageCreate
-	MessageUpdate *discordgo.MessageUpdate
-	MessageDelete *discordgo.MessageDelete
+	ChannelCreate            *discordgo.ChannelCreate
+	ChannelDelete            *discordgo.ChannelDelete
+	ChannelPinsUpdate        *discordgo.ChannelPinsUpdate
+	ChannelUpdate            *discordgo.ChannelUpdate
+	GuildBanAdd              *discordgo.GuildBanAdd
+	GuildBanRemove           *discordgo.GuildBanRemove
+	GuildCreate              *discordgo.GuildCreate
+	GuildDelete              *discordgo.GuildDelete
+	GuildEmojisUpdate        *discordgo.GuildEmojisUpdate
+	GuildMemberAdd           *discordgo.GuildMemberAdd
+	GuildMemberRemove        *discordgo.GuildMemberRemove
+	GuildMemberUpdate        *discordgo.GuildMemberUpdate
+	GuildMembersChunk        *discordgo.GuildMembersChunk
+	GuildRoleCreate          *discordgo.GuildRoleCreate
+	GuildRoleDelete          *discordgo.GuildRoleDelete
+	GuildRoleUpdate          *discordgo.GuildRoleUpdate
+	GuildUpdate              *discordgo.GuildUpdate
+	MessageCreate            *discordgo.MessageCreate
+	MessageDelete            *discordgo.MessageDelete
+	MessageReactionAdd       *discordgo.MessageReactionAdd
+	MessageReactionRemove    *discordgo.MessageReactionRemove
+	MessageReactionRemoveAll *discordgo.MessageReactionRemoveAll
+	MessageUpdate            *discordgo.MessageUpdate
+	PresenceUpdate           *discordgo.PresenceUpdate
+	GuildIntegrationsUpdate  *discordgo.GuildIntegrationsUpdate
+	MessageDeleteBulk        *discordgo.MessageDeleteBulk
+	UserUpdate               *discordgo.UserUpdate
+	VoiceStateUpdate         *discordgo.VoiceStateUpdate
+	VoiceServerUpdate        *discordgo.VoiceServerUpdate
+	WebhooksUpdate           *discordgo.WebhooksUpdate
 }
 
 // GenerateRoutingKey generates an Routing Key for AMQP based on a Event Type
@@ -36,6 +91,7 @@ func GenerateRoutingKey(eventType Type) string {
 }
 
 // GenerateEventFromDiscordgoEvent generates an Event from a Discordgo Event
+// nolint: gocyclo
 func GenerateEventFromDiscordgoEvent(botUserID string, eventItem interface{}) (*Event, error) {
 	event := &Event{
 		ReceivedAt: time.Now(),
@@ -43,22 +99,104 @@ func GenerateEventFromDiscordgoEvent(botUserID string, eventItem interface{}) (*
 	}
 
 	switch t := eventItem.(type) {
+	case *discordgo.GuildCreate:
+		event.Type = GuildCreateType
+		event.GuildCreate = t
+	case *discordgo.GuildUpdate:
+		event.Type = GuildUpdateType
+		event.GuildUpdate = t
+	case *discordgo.GuildDelete:
+		event.Type = GuildDeleteType
+		event.GuildDelete = t
+	case *discordgo.GuildMemberAdd:
+		event.Type = GuildMemberAddType
+		event.GuildMemberAdd = t
+	case *discordgo.GuildMemberUpdate:
+		event.Type = GuildMemberUpdateType
+		event.GuildMemberUpdate = t
+	case *discordgo.GuildMemberRemove:
+		event.Type = GuildMemberRemoveType
+		event.GuildMemberRemove = t
+	case *discordgo.GuildMembersChunk:
+		event.Type = GuildMembersChunkType
+		event.GuildMembersChunk = t
+	case *discordgo.GuildRoleCreate:
+		event.Type = GuildRoleCreateType
+		event.GuildRoleCreate = t
+	case *discordgo.GuildRoleUpdate:
+		event.Type = GuildRoleUpdateType
+		event.GuildRoleUpdate = t
+	case *discordgo.GuildRoleDelete:
+		event.Type = GuildRoleDeleteType
+		event.GuildRoleDelete = t
+	case *discordgo.GuildEmojisUpdate:
+		event.Type = GuildEmojisUpdateType
+		event.GuildEmojisUpdate = t
+	case *discordgo.ChannelCreate:
+		event.Type = ChannelCreateType
+		event.ChannelCreate = t
+	case *discordgo.ChannelUpdate:
+		event.Type = ChannelUpdateType
+		event.ChannelUpdate = t
+	case *discordgo.ChannelDelete:
+		event.Type = ChannelDeleteType
+		event.ChannelDelete = t
 	case *discordgo.MessageCreate:
-		event.Type = MessageCreateEventType
+		event.Type = MessageCreateType
 		event.MessageCreate = t
-
-		return event, nil
 	case *discordgo.MessageUpdate:
-		event.Type = MessageUpdateEventType
+		event.Type = MessageUpdateType
 		event.MessageUpdate = t
-
-		return event, nil
 	case *discordgo.MessageDelete:
-		event.Type = MessageDeleteEventType
+		event.Type = MessageDeleteType
 		event.MessageDelete = t
+	case *discordgo.ChannelPinsUpdate:
+		event.Type = ChannelPinsUpdateType
+		event.ChannelPinsUpdate = t
+	case *discordgo.GuildBanAdd:
+		event.Type = GuildBanAddType
+		event.GuildBanAdd = t
+	case *discordgo.GuildBanRemove:
+		event.Type = GuildBanRemoveType
+		event.GuildBanRemove = t
+	case *discordgo.MessageReactionAdd:
+		event.Type = MessageReactionAddType
+		event.MessageReactionAdd = t
+	case *discordgo.MessageReactionRemove:
+		event.Type = MessageReactionRemoveType
+		event.MessageReactionRemove = t
+	case *discordgo.MessageReactionRemoveAll:
+		event.Type = MessageReactionRemoveAllType
+		event.MessageReactionRemoveAll = t
+	case *discordgo.GuildIntegrationsUpdate:
+		event.Type = GuildIntegrationsUpdateType
+		event.GuildIntegrationsUpdate = t
+	case *discordgo.PresenceUpdate:
+		event.Type = PresenceUpdateType
+		event.PresenceUpdate = t
+	case *discordgo.MessageDeleteBulk:
+		event.Type = MessageDeleteBulkType
+		event.MessageDeleteBulk = t
+	case *discordgo.UserUpdate:
+		event.Type = UserUpdateType
+		event.UserUpdate = t
+	case *discordgo.VoiceStateUpdate:
+		event.Type = VoiceStateUpdateType
+		event.VoiceStateUpdate = t
+	case *discordgo.VoiceServerUpdate:
+		event.Type = VoiceServerUpdateType
+		event.VoiceServerUpdate = t
+	case *discordgo.WebhooksUpdate:
+		event.Type = WebhooksUpdateType
+		event.WebhooksUpdate = t
+	case *discordgo.TypingStart, *discordgo.Ready, *discordgo.Event, *discordgo.Connect:
+		// ignored events
+		return nil, nil
+	}
 
+	if event.Type != "" {
 		return event, nil
 	}
 
-	return nil, errors.New("event type is not supported")
+	return nil, errors.New("received unexpected event")
 }
