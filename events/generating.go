69dbea5083b9f463fe97d9a2912aca4b580b1edf
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-02-16 20:21:27 +0100

    improves event hashing

--- events/generating.go
+++ events/generating.go
@@ -5,6 +5,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/bwmarrin/discordgo"
@@ -12,6 +13,8 @@ import (
 	// nolint: gosec
 	"crypto/md5"
 )
+
+// TODO: fix event struct hashing
 
 // GenerateRoutingKey generates an Routing Key for AMQP based on a Event Type
 func GenerateRoutingKey(eventType Type) string {
@@ -31,93 +34,213 @@ func GenerateEventFromDiscordgoEvent(botUserID string, eventItem interface{}) (*
 	case *discordgo.GuildCreate:
 		event.Type = GuildCreateType
 		event.GuildCreate = t
+		event.ID, err = hash(t.ID)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.GuildUpdate:
 		event.Type = GuildUpdateType
 		event.GuildUpdate = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.GuildDelete:
 		event.Type = GuildDeleteType
 		event.GuildDelete = t
+		event.ID, err = hash(t.ID)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.GuildMemberAdd:
 		event.Type = GuildMemberAddType
 		event.GuildMemberAdd = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.GuildMemberUpdate:
 		event.Type = GuildMemberUpdateType
 		event.GuildMemberUpdate = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.GuildMemberRemove:
 		event.Type = GuildMemberRemoveType
 		event.GuildMemberRemove = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.GuildMembersChunk:
 		event.Type = GuildMembersChunkType
 		event.GuildMembersChunk = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.GuildRoleCreate:
 		event.Type = GuildRoleCreateType
 		event.GuildRoleCreate = t
+		event.ID, err = hash(t.Role.ID)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.GuildRoleUpdate:
 		event.Type = GuildRoleUpdateType
 		event.GuildRoleUpdate = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.GuildRoleDelete:
 		event.Type = GuildRoleDeleteType
 		event.GuildRoleDelete = t
+		event.ID, err = hash(t.RoleID)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.GuildEmojisUpdate:
 		event.Type = GuildEmojisUpdateType
 		event.GuildEmojisUpdate = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.ChannelCreate:
 		event.Type = ChannelCreateType
 		event.ChannelCreate = t
+		event.ID, err = hash(t.ID)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.ChannelUpdate:
 		event.Type = ChannelUpdateType
 		event.ChannelUpdate = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.ChannelDelete:
 		event.Type = ChannelDeleteType
 		event.ChannelDelete = t
+		event.ID, err = hash(t.ID)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.MessageCreate:
 		event.Type = MessageCreateType
 		event.MessageCreate = t
+		event.ID, err = hash(t.ID)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.MessageUpdate:
 		event.Type = MessageUpdateType
 		event.MessageUpdate = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.MessageDelete:
 		event.Type = MessageDeleteType
 		event.MessageDelete = t
+		event.ID, err = hash(t.ID)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.ChannelPinsUpdate:
 		event.Type = ChannelPinsUpdateType
 		event.ChannelPinsUpdate = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.GuildBanAdd:
 		event.Type = GuildBanAddType
 		event.GuildBanAdd = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.GuildBanRemove:
 		event.Type = GuildBanRemoveType
 		event.GuildBanRemove = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.MessageReactionAdd:
 		event.Type = MessageReactionAddType
 		event.MessageReactionAdd = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.MessageReactionRemove:
 		event.Type = MessageReactionRemoveType
 		event.MessageReactionRemove = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.MessageReactionRemoveAll:
 		event.Type = MessageReactionRemoveAllType
 		event.MessageReactionRemoveAll = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.GuildIntegrationsUpdate:
 		event.Type = GuildIntegrationsUpdateType
 		event.GuildIntegrationsUpdate = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.PresenceUpdate:
 		event.Type = PresenceUpdateType
 		event.PresenceUpdate = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.MessageDeleteBulk:
 		event.Type = MessageDeleteBulkType
 		event.MessageDeleteBulk = t
+		event.ID, err = hash(strings.Join(t.Messages, ""))
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.UserUpdate:
 		event.Type = UserUpdateType
 		event.UserUpdate = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.VoiceStateUpdate:
 		event.Type = VoiceStateUpdateType
 		event.VoiceStateUpdate = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.VoiceServerUpdate:
 		event.Type = VoiceServerUpdateType
 		event.VoiceServerUpdate = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.WebhooksUpdate:
 		event.Type = WebhooksUpdateType
 		event.WebhooksUpdate = t
+		event.ID, err = hash(t)
+		if err != nil {
+			return nil, err
+		}
 	case *discordgo.TypingStart, *discordgo.Ready, *discordgo.Event, *discordgo.Connect:
 		// ignored events
 		return nil, nil
@@ -127,10 +250,6 @@ func GenerateEventFromDiscordgoEvent(botUserID string, eventItem interface{}) (*
 		return nil, errors.New("received unexpected event")
 	}
 
-	event.ID, err = hash(eventItem)
-	if err != nil {
-		return nil, err
-	}
 	return event, nil
 }
 
