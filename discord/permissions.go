d1dc01e13f915b57d89c78b94215bb4d56ff2f9d
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-02-25 15:06:54 +0100

    adds event.RequireOr()

--- discord/permissions.go
+++ discord/permissions.go
@@ -5,7 +5,7 @@ import (
 	"gitlab.com/Cacophony/go-kit/state"
 )
 
-// UserHasPermission returns true if the User has all given permissions in the given channel
+// UserHasPermission returns true if the User has all of th egiven permissions in the given channel
 func UserHasPermission(
 	state *state.State, userID, channelID string, firstPermission int, permissions ...int,
 ) bool {
@@ -30,6 +30,33 @@ func UserHasPermission(
 	}
 
 	return true
+}
+
+// UserHasPermissionOr returns true if the User has any of the given permissions in the given channel
+func UserHasPermissionOr(
+	state *state.State, userID, channelID string, firstPermission int, permissions ...int,
+) bool {
+	if userID == "" || channelID == "" {
+		return false
+	}
+
+	if state == nil {
+		return false
+	}
+
+	userChannelPermissions, err := state.UserChannelPermissions(userID, channelID)
+	if err != nil {
+		return false
+	}
+
+	for _, permission := range append(permissions, firstPermission) {
+
+		if userChannelPermissions&permission == permission {
+			return true
+		}
+	}
+
+	return false
 }
 
 // PermissionName returns a human readable name for a permission
--- events/event_permissions.go
+++ events/event_permissions.go
@@ -8,7 +8,9 @@ import (
 
 // Require calls the callback if the user has all required permissions
 func (e *Event) Require(callback func(), firstPermission int, permissions ...int) {
-	if !discord.UserHasPermission(e.state, e.UserID, e.ChannelID, firstPermission, permissions...) {
+	if !discord.UserHasPermission(
+		e.state, e.UserID, e.ChannelID, firstPermission, permissions...,
+	) {
 		var permissionsText string
 		for _, permission := range append(permissions, firstPermission) {
 			permissionsText += discord.PermissionName(permission) + ", "
@@ -22,4 +24,28 @@ func (e *Event) Require(callback func(), firstPermission int, permissions ...int
 		e.Respond(message) // nolint: errcheck
 		return
 	}
+
+	callback()
+}
+
+// Require calls the callback if the user has all required permissions
+func (e *Event) RequireOr(callback func(), firstPermission int, permissions ...int) {
+	if !discord.UserHasPermissionOr(
+		e.state, e.UserID, e.ChannelID, firstPermission, permissions...,
+	) {
+		var permissionsText string
+		for _, permission := range append(permissions, firstPermission) {
+			permissionsText += discord.PermissionName(permission) + ", "
+		}
+		permissionsText = strings.TrimRight(permissionsText, ", ")
+
+		message := "**Looks like you do not have the right permissions to do this.**" +
+			"\nYou need the following permission:" +
+			"\n```\n" + permissionsText + "\n```"
+
+		e.Respond(message) // nolint: errcheck
+		return
+	}
+
+	callback()
 }
