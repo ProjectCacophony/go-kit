4cf02f5c3b228596e9c8b076bda6e91c8dc69fed
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-02-27 18:33:46 +0100

    adds event.Has(), and event.HasOr()

--- events/event_permissions.go
+++ events/event_permissions.go
@@ -6,32 +6,68 @@ import (
 	"gitlab.com/Cacophony/go-kit/permissions"
 )
 
+// Has returns true if the user has all the required permissions
+func (e *Event) Has(
+	firstPermission permissions.PermissionInterface,
+	permissions ...permissions.PermissionInterface,
+) bool {
+	if e.UserID == "" {
+		return false
+	}
+
+	for _, permission := range append(permissions, firstPermission) {
+		if permission.Match(e.state, e.BotOwnerIDs(), e.UserID, e.ChannelID) {
+			continue
+		}
+
+		return false
+	}
+
+	return true
+}
+
+// HasOr returns true if the user has one of the required permissions
+func (e *Event) HasOr(
+	firstPermission permissions.PermissionInterface,
+	permissions ...permissions.PermissionInterface,
+) bool {
+	if e.UserID == "" {
+		return false
+	}
+
+	for _, permission := range append(permissions, firstPermission) {
+		if !permission.Match(e.state, e.BotOwnerIDs(), e.UserID, e.ChannelID) {
+			continue
+		}
+
+		return true
+	}
+
+	return false
+}
+
 // Require calls the callback if the user has all of the required permissions
 func (e *Event) Require(
 	callback func(),
 	firstPermission permissions.PermissionInterface,
 	permissions ...permissions.PermissionInterface,
 ) {
-	for _, permission := range append(permissions, firstPermission) {
-		if permission.Match(e.state, e.BotOwnerIDs(), e.UserID, e.ChannelID) {
-			continue
-		}
-
-		var permissionsText string
-		for _, permission := range append(permissions, firstPermission) {
-			permissionsText += permission.Name() + ", "
-		}
-		permissionsText = strings.TrimRight(permissionsText, ", ")
-
-		message := "**Looks like you do not have the right permissions to do this.**" +
-			"\nYou need all of the following permissions:" +
-			"\n```\n" + permissionsText + "\n```"
-
-		e.Respond(message) // nolint: errcheck
+	if e.Has(firstPermission, permissions...) {
+		callback()
 		return
 	}
 
-	callback()
+	var permissionsText string
+	for _, permission := range append(permissions, firstPermission) {
+		permissionsText += permission.Name() + ", "
+	}
+	permissionsText = strings.TrimRight(permissionsText, ", ")
+
+	message := "**Looks like you do not have the right permissions to do this.**" +
+		"\nYou need all of the following permissions:" +
+		"\n```\n" + permissionsText + "\n```"
+
+	e.Respond(message) // nolint: errcheck
 }
 
 // RequireOr calls the callback if the user has one of the required permissions
@@ -40,32 +76,20 @@ func (e *Event) RequireOr(
 	firstPermission permissions.PermissionInterface,
 	permissions ...permissions.PermissionInterface,
 ) {
-	var matched bool
-
-	for _, permission := range append(permissions, firstPermission) {
-		if !permission.Match(e.state, e.BotOwnerIDs(), e.UserID, e.ChannelID) {
-			continue
-		}
-
-		matched = true
-		break
-	}
-
-	if !matched {
-
-		var permissionsText string
-		for _, permission := range append(permissions, firstPermission) {
-			permissionsText += permission.Name() + ", "
-		}
-		permissionsText = strings.TrimRight(permissionsText, ", ")
-
-		message := "**Looks like you do not have the right permissions to do this.**" +
-			"\nYou need one of the following permissions:" +
-			"\n```\n" + permissionsText + "\n```"
-
-		e.Respond(message) // nolint: errcheck
+	if e.HasOr(firstPermission, permissions...) {
+		callback()
 		return
 	}
 
-	callback()
+	var permissionsText string
+	for _, permission := range append(permissions, firstPermission) {
+		permissionsText += permission.Name() + ", "
+	}
+	permissionsText = strings.TrimRight(permissionsText, ", ")
+
+	message := "**Looks like you do not have the right permissions to do this.**" +
+		"\nYou need one of the following permissions:" +
+		"\n```\n" + permissionsText + "\n```"
+
+	e.Respond(message) // nolint: errcheck
 }
