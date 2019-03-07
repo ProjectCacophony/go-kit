815c18bf3ab2bc7434af59a0d78f4071f750619c
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-03-07 21:48:37 +0100

    event: reverts useless DynamoDB marshalling configuration

--- events/event.go
+++ events/event.go
@@ -29,38 +29,38 @@ type Event struct {
 	UserID     string    `json:"user_id,omitempty"`
 
 	// discordgo event data
-	ChannelCreate            *discordgo.ChannelCreate            `json:"discord_channel_create,omitempty" dynamodbav:"discord_channel_create,binaryset"`
-	ChannelDelete            *discordgo.ChannelDelete            `json:"discord_channel_delete,omitempty" dynamodbav:"discord_channel_delete,binaryset"`
-	ChannelPinsUpdate        *discordgo.ChannelPinsUpdate        `json:"discord_channel_pins_update,omitempty" dynamodbav:"discord_channel_pins_update,binaryset"`
-	ChannelUpdate            *discordgo.ChannelUpdate            `json:"discord_channel_update,omitempty" dynamodbav:"discord_channel_update,binaryset"`
-	GuildBanAdd              *discordgo.GuildBanAdd              `json:"discord_guild_ban_add,omitempty" dynamodbav:"discord_guild_ban_add,binaryset"`
-	GuildBanRemove           *discordgo.GuildBanRemove           `json:"discord_guild_ban_remove,omitempty" dynamodbav:"discord_guild_ban_remove,binaryset"`
-	GuildCreate              *discordgo.GuildCreate              `json:"discord_guild_create,omitempty" dynamodbav:"discord_guild_create,binaryset"`
-	GuildDelete              *discordgo.GuildDelete              `json:"discord_guild_delete,omitempty" dynamodbav:"discord_guild_delete,binaryset"`
-	GuildEmojisUpdate        *discordgo.GuildEmojisUpdate        `json:"discord_guild_emojis_update,omitempty" dynamodbav:"discord_guild_emojis_update,binaryset"`
-	GuildMemberAdd           *discordgo.GuildMemberAdd           `json:"discord_guild_member_add,omitempty" dynamodbav:"discord_guild_member_add,binaryset"`
-	GuildMemberRemove        *discordgo.GuildMemberRemove        `json:"discord_guild_member_remove,omitempty" dynamodbav:"discord_guild_member_remove,binaryset"`
-	GuildMemberUpdate        *discordgo.GuildMemberUpdate        `json:"discord_guild_member_update,omitempty" dynamodbav:"discord_guild_member_update,binaryset"`
-	GuildMembersChunk        *discordgo.GuildMembersChunk        `json:"discord_guild_members_chunk,omitempty" dynamodbav:"discord_guild_members_chunk,binaryset"`
-	GuildRoleCreate          *discordgo.GuildRoleCreate          `json:"discord_guild_role_create,omitempty" dynamodbav:"discord_guild_role_create,binaryset"`
-	GuildRoleDelete          *discordgo.GuildRoleDelete          `json:"discord_guild_role_delete,omitempty" dynamodbav:"discord_guild_role_delete,binaryset"`
-	GuildRoleUpdate          *discordgo.GuildRoleUpdate          `json:"discord_guild_role_update,omitempty" dynamodbav:"discord_guild_role_update,binaryset"`
-	GuildUpdate              *discordgo.GuildUpdate              `json:"discord_guild_update,omitempty" dynamodbav:"discord_guild_update,binaryset"`
-	MessageCreate            *discordgo.MessageCreate            `json:"discord_message_create,omitempty" dynamodbav:"discord_message_create,binaryset"`
-	MessageDelete            *discordgo.MessageDelete            `json:"discord_message_delete,omitempty" dynamodbav:"discord_message_delete,binaryset"`
-	MessageReactionAdd       *discordgo.MessageReactionAdd       `json:"discord_message_reaction_add,omitempty" dynamodbav:"discord_message_reaction_add,binaryset"`
-	MessageReactionRemove    *discordgo.MessageReactionRemove    `json:"discord_message_reaction_remove,omitempty" dynamodbav:"discord_message_reaction_remove,binaryset"`
-	MessageReactionRemoveAll *discordgo.MessageReactionRemoveAll `json:"discord_message_reaction_remove_all,omitempty" dynamodbav:"discord_message_reaction_remove_all,binaryset"`
-	MessageUpdate            *discordgo.MessageUpdate            `json:"discord_message_update,omitempty" dynamodbav:"discord_message_update,binaryset"`
-	PresenceUpdate           *discordgo.PresenceUpdate           `json:"discord_presence_update,omitempty" dynamodbav:"discord_presence_update,binaryset"`
-	GuildIntegrationsUpdate  *discordgo.GuildIntegrationsUpdate  `json:"discord_guild_integrations_update,omitempty" dynamodbav:"discord_guild_integrations_update,binaryset"`
-	MessageDeleteBulk        *discordgo.MessageDeleteBulk        `json:"discord_message_delete_bulk,omitempty" dynamodbav:"discord_message_delete_bulk,binaryset"`
-	UserUpdate               *discordgo.UserUpdate               `json:"discord_user_update,omitempty" dynamodbav:"discord_user_update,binaryset"`
-	VoiceStateUpdate         *discordgo.VoiceStateUpdate         `json:"discord_voice_state_update,omitempty" dynamodbav:"discord_voice_state_update,binaryset"`
-	VoiceServerUpdate        *discordgo.VoiceServerUpdate        `json:"discord_voice_server_update,omitempty" dynamodbav:"discord_voice_server_update,binaryset"`
-	WebhooksUpdate           *discordgo.WebhooksUpdate           `json:"discord_webhooks_update,omitempty" dynamodbav:"discord_webhooks_update,binaryset"`
+	ChannelCreate            *discordgo.ChannelCreate            `json:"discord_channel_create,omitempty"`
+	ChannelDelete            *discordgo.ChannelDelete            `json:"discord_channel_delete,omitempty"`
+	ChannelPinsUpdate        *discordgo.ChannelPinsUpdate        `json:"discord_channel_pins_update,omitempty"`
+	ChannelUpdate            *discordgo.ChannelUpdate            `json:"discord_channel_update,omitempty"`
+	GuildBanAdd              *discordgo.GuildBanAdd              `json:"discord_guild_ban_add,omitempty"`
+	GuildBanRemove           *discordgo.GuildBanRemove           `json:"discord_guild_ban_remove,omitempty"`
+	GuildCreate              *discordgo.GuildCreate              `json:"discord_guild_create,omitempty"`
+	GuildDelete              *discordgo.GuildDelete              `json:"discord_guild_delete,omitempty"`
+	GuildEmojisUpdate        *discordgo.GuildEmojisUpdate        `json:"discord_guild_emojis_update,omitempty"`
+	GuildMemberAdd           *discordgo.GuildMemberAdd           `json:"discord_guild_member_add,omitempty"`
+	GuildMemberRemove        *discordgo.GuildMemberRemove        `json:"discord_guild_member_remove,omitempty"`
+	GuildMemberUpdate        *discordgo.GuildMemberUpdate        `json:"discord_guild_member_update,omitempty"`
+	GuildMembersChunk        *discordgo.GuildMembersChunk        `json:"discord_guild_members_chunk,omitempty"`
+	GuildRoleCreate          *discordgo.GuildRoleCreate          `json:"discord_guild_role_create,omitempty"`
+	GuildRoleDelete          *discordgo.GuildRoleDelete          `json:"discord_guild_role_delete,omitempty"`
+	GuildRoleUpdate          *discordgo.GuildRoleUpdate          `json:"discord_guild_role_update,omitempty"`
+	GuildUpdate              *discordgo.GuildUpdate              `json:"discord_guild_update,omitempty"`
+	MessageCreate            *discordgo.MessageCreate            `json:"discord_message_create,omitempty"`
+	MessageDelete            *discordgo.MessageDelete            `json:"discord_message_delete,omitempty"`
+	MessageReactionAdd       *discordgo.MessageReactionAdd       `json:"discord_message_reaction_add,omitempty"`
+	MessageReactionRemove    *discordgo.MessageReactionRemove    `json:"discord_message_reaction_remove,omitempty"`
+	MessageReactionRemoveAll *discordgo.MessageReactionRemoveAll `json:"discord_message_reaction_remove_all,omitempty"`
+	MessageUpdate            *discordgo.MessageUpdate            `json:"discord_message_update,omitempty"`
+	PresenceUpdate           *discordgo.PresenceUpdate           `json:"discord_presence_update,omitempty"`
+	GuildIntegrationsUpdate  *discordgo.GuildIntegrationsUpdate  `json:"discord_guild_integrations_update,omitempty"`
+	MessageDeleteBulk        *discordgo.MessageDeleteBulk        `json:"discord_message_delete_bulk,omitempty"`
+	UserUpdate               *discordgo.UserUpdate               `json:"discord_user_update,omitempty"`
+	VoiceStateUpdate         *discordgo.VoiceStateUpdate         `json:"discord_voice_state_update,omitempty"`
+	VoiceServerUpdate        *discordgo.VoiceServerUpdate        `json:"discord_voice_server_update,omitempty"`
+	WebhooksUpdate           *discordgo.WebhooksUpdate           `json:"discord_webhooks_update,omitempty"`
 	// Cacophony Event Data
-	BucketUpdate *BucketUpdate `json:"cacophony_bucket_update,omitempty" dynamodbav:"cacophony_bucket_update,binaryset"`
+	BucketUpdate *BucketUpdate `json:"cacophony_bucket_update,omitempty"`
 
 	// non marshalled events
 
