d83846117b47270368ff44ba918c88ec659b8d4d
Snakeyez <JohnBar216@gmail.com>
2019-05-11 00:04:45 -0500

    localization: removed duplicate function

--- events/event_translate.go
+++ events/event_translate.go
@@ -1,24 +1,11 @@
 package events
 
+import (
+	"gitlab.com/Cacophony/go-kit/localisation"
+)
+
 // Translate translates a given key for the event
 func (e *Event) Translate(key string, values ...interface{}) string {
-	if key == "" {
-		return key
-	}
-
-	values = append(values,
-		"prefix", e.prefix,
-	)
-
-	var translation string
-	for _, localisation := range e.Localisations() {
-		translation = localisation.Translatef(key, values...)
-
-		// valid translation found
-		if translation != key {
-			return translation
-		}
-	}
-
-	return key
+	values = append(values, "prefix", e.prefix)
+	return localisation.Translate(e.Localisations(), key, values)
 }
