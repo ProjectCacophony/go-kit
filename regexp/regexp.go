610c4fbc93a89a7d1fb42da8b8a439e4f5deece3
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-06-16 11:51:33 +0200

    regexp: discord invite adds support for trailing /

--- regexp/regexp.go
+++ regexp/regexp.go
@@ -7,6 +7,6 @@ import (
 var (
 	// DiscordInviteRegexp matches a linked Discord Invite
 	DiscordInviteRegexp = regexp.MustCompile(
-		`^(http(s)?:\/\/)?(discord\.gg(\/invite)?|discordapp\.com\/invite)\/([A-Za-z0-9-]+)$`,
+		`^(http(s)?:\/\/)?(discord\.gg(\/invite)?|discordapp\.com\/invite)\/([A-Za-z0-9-]+)(\/?)$`,
 	)
 )
