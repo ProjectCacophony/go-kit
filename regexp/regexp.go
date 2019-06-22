d62ef1462d4759a72d2033e472b8a5409cfc472f
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-06-22 12:36:24 +0200

    regexp: adds `DiscordEmojiRegexp`

--- regexp/regexp.go
+++ regexp/regexp.go
@@ -9,4 +9,7 @@ var (
 	DiscordInviteRegexp = regexp.MustCompile(
 		`^(http(s)?:\/\/)?(discord\.gg(\/invite)?|discordapp\.com\/invite)\/([A-Za-z0-9-]+)(\/?)$`,
 	)
+
+	// DiscordEmojiRegexp matches an Emoji, or an Emoji ID
+	DiscordEmojiRegexp = regexp.MustCompile(`(<(a)?:([^<>:]+):)?([0-9]+)>?`)
 )
