50bba503549d448081fb0f708614e842d68d397f
Snakeyez <JohnBar216@gmail.com>
2019-08-25 19:02:05 -0500

    config: Adds user config

---
+++ config/user.go
@@ -0,0 +1,77 @@
+package config
+
+import (
+	"errors"
+
+	"github.com/jinzhu/gorm"
+)
+
+func userNamespace(userID string) string {
+	return "user:" + userID
+}
+
+func UserSetString(db *gorm.DB, userID, key, value string) error {
+	if userID == "" {
+		return errors.New("invalid User ID")
+	}
+	if key == "" {
+		return errors.New("invalid configuration key")
+	}
+
+	return setString(db, userNamespace(userID), key, value)
+}
+
+func UserGetString(db *gorm.DB, userID, key string) (string, error) {
+	if userID == "" {
+		return "", errors.New("invalid User ID")
+	}
+	if key == "" {
+		return "", errors.New("invalid configuration key")
+	}
+
+	return getString(db, userNamespace(userID), key)
+}
+
+func UserSetBool(db *gorm.DB, userID, key string, value bool) error {
+	if userID == "" {
+		return errors.New("invalid User ID")
+	}
+	if key == "" {
+		return errors.New("invalid configuration key")
+	}
+
+	return setBool(db, userNamespace(userID), key, value)
+}
+
+func UserGetBool(db *gorm.DB, userID, key string) (bool, error) {
+	if userID == "" {
+		return false, errors.New("invalid User ID")
+	}
+	if key == "" {
+		return false, errors.New("invalid configuration key")
+	}
+
+	return getBool(db, userNamespace(userID), key)
+}
+
+func UserSetInterface(db *gorm.DB, userID, key string, value interface{}) error {
+	if userID == "" {
+		return errors.New("invalid User ID")
+	}
+	if key == "" {
+		return errors.New("invalid configuration key")
+	}
+
+	return setInterface(db, userNamespace(userID), key, value)
+}
+
+func UserGetInterface(db *gorm.DB, userID, key string, value interface{}) error {
+	if userID == "" {
+		return errors.New("invalid User ID")
+	}
+	if key == "" {
+		return errors.New("invalid configuration key")
+	}
+
+	return getInterface(db, userNamespace(userID), key, value)
+}
