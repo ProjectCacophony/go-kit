c0e30e3ac9d32761a92791ddb8040b5b63556abe
Sebastian Winkler <sebastian.winkler@slmn.de>
2019-10-27 21:12:57 +0100

    state: removes obsolete GuildInvites parameter

--- state/getters.go
+++ state/getters.go
@@ -287,7 +287,7 @@ func (s *State) GuildWebhooks(guildID string) (webhooks []*discordgo.Webhook, er
 	return webhooks, nil
 }
 
-func (s *State) GuildInvites(guildID string, refresh bool) (invites []*discordgo.Invite, err error) {
+func (s *State) GuildInvites(guildID string) (invites []*discordgo.Invite, err error) {
 	data, err := readStateObject(s.client, guildInvitesKey(guildID))
 	if err != nil {
 		if errors.Is(err, redis.Nil) {
