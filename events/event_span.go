ca66cd23f2e37b19625bff8839be98f26f416371
Sebastian Winkler <sebastian@winkler.dev>
2020-10-17 20:21:57 +0200

    events: exports span labels

--- events/event_span.go
+++ events/event_span.go
@@ -1,5 +1,9 @@
 package events
 
+import (
+	"go.opentelemetry.io/otel/label"
+)
+
 type SpanContext struct {
 	Values map[string]string `json:"values"`
 }
@@ -18,4 +22,15 @@ func (sp *SpanContext) Set(key, value string) {
 	}
 
 	sp.Values[key] = value
+
 }
+
+var (
+	SpanLabelEventingType      = label.Key("eventing_type")
+	SpanLabelEventingIsCommand = label.Key("eventing_is_command")
+	SpanLabelDiscordBotUserID  = label.Key("discord_bot_user_id")
+	SpanLabelDiscordGuildID    = label.Key("discord_guild_id")
+	SpanLabelDiscordChannelID  = label.Key("discord_channel_id")
+	SpanLabelDiscordUserID     = label.Key("discord_user_id")
+	SpanLabelDiscordMessageID  = label.Key("discord_message_id")
+)
